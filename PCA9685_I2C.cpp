6166f8792d6e987f8487e6776cc041ace918b7ff
Paul <101666643+Scar092@users.noreply.github.com>
2022-07-02 03:16:30 +0300

    Update PCA9685_I2C.cpp
    Add

--- PCA9685_I2C.cpp
+++ PCA9685_I2C.cpp
@@ -1,3 +1,17 @@
 #include <wiringPiI2C.h>
 
-#define
+#define BOARD_ID 0x60 //device adress
+#define LED13_ON_L 0x3A
+#define LED13_ON_H 0x3B
+#define LED13_OFF_L 0x3C
+#define LED13_OFF_H 0x3D
+
+int board, default_1, default_2, result_1, result_2;
+char val_l, val_h;
+
+board = wiringPiI2CSetup (BOARD_ID);
+default_1 = wiringPiI2CWriteReg8 (BOARD_ID, LED13_ON_L, 0xA);
+default_2 = wiringPiI2CWriteReg8 (BOARD_ID, LED13_ON_H, 0x0);
+
+result_1 = wiringPiI2CWriteReg8 (BOARD_ID, LED13_OFF_L, val_l);
+result_1 = wiringPiI2CWriteReg8 (BOARD_ID, LED13_OFF_H, val_h);
