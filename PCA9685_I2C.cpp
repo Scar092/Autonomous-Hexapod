0eb3ccdba0641a1674b4dedd42134127152ba16f
Paul <101666643+Scar092@users.noreply.github.com>
2022-07-02 04:28:45 +0300

    Update PCA9685_I2C.cpp

--- PCA9685_I2C.cpp
+++ PCA9685_I2C.cpp
@@ -6,29 +6,32 @@
 #define LED13_OFF_L 0x3C //low OFF register date
 #define LED13_OFF_H 0x3D //high OFF register date
 
-int board, default_1, default_2, result_1, result_2;
-int ang, pwm
-char val_l, val_h;
+void set_servo (ang) {
 
-ang = ; //angle defined early 
-pwm = angle/180*4095; //angle to PWM
-val_h = pwm & 0xF00; //high 4 bits of PWM
-val_l = pwm & 0xFF; //low 8 bits of PWM
+     int board, default_1, default_2, result_1, result_2;
+     int ang, pwm
+     char val_l, val_h;
 
-board = wiringPiI2CSetup (BOARD_ID); //initialisation
+     ang = ;
+     pwm = angle/180*4095; //angle to PWM
+     val_h = pwm & 0xF00; //high 4 bits of PWM
+     val_l = pwm & 0xFF; //low 8 bits of PWM
 
-/* Each Servo (LED) output have two 12-bit words, that indicates
-front and rear moments of every pulse (called ON and OFF moments).
-There are 4096 repeating parts of time. Every 10 part we will have
-start condition. And the pulse width will be identified by end
-moment. Every word is devided between 2 registers. So we have 8 low
-bits and 4 high bits. Also have additional bits in  High register.
-Please, look to PCA9685 datasheet. And be attentive. */ 
+     board = wiringPiI2CSetup (BOARD_ID); //initialisation
 
-//Start condition 
-default_1 = wiringPiI2CWriteReg8 (BOARD_ID, LED13_ON_L, 0xA);
-default_2 = wiringPiI2CWriteReg8 (BOARD_ID, LED13_ON_H, 0x0);
+     /* Each Servo (LED) output have two 12-bit words, that indicates
+     front and rear moments of every pulse (called ON and OFF moments).
+     There are 4096 repeating parts of time. Every 10 part we will have
+     start condition. And the pulse width will be identified by end
+     moment. Every word is devided between 2 registers. So we have 8 low
+     bits and 4 high bits. Also have additional bits in  High register.
+     Please, look to PCA9685 datasheet. And be attentive. */ 
 
-//Stop condition
-result_1 = wiringPiI2CWriteReg8 (BOARD_ID, LED13_OFF_L, val_l);
-result_1 = wiringPiI2CWriteReg8 (BOARD_ID, LED13_OFF_H, val_h);
+     //Start condition 
+     default_1 = wiringPiI2CWriteReg8 (BOARD_ID, LED13_ON_L, 0xA);
+     default_2 = wiringPiI2CWriteReg8 (BOARD_ID, LED13_ON_H, 0x0);
+
+     //Stop condition
+     result_1 = wiringPiI2CWriteReg8 (BOARD_ID, LED13_OFF_L, val_l);
+     result_1 = wiringPiI2CWriteReg8 (BOARD_ID, LED13_OFF_H, val_h);
+}
