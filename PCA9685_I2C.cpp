16c13142ab7cff1847b534d6c2b99a8c80623f93
Paul <101666643+Scar092@users.noreply.github.com>
2022-07-08 15:06:30 +0300

    Update PCA9685_I2C.cpp

--- PCA9685_I2C.cpp
+++ PCA9685_I2C.cpp
@@ -1,97 +1,28 @@
 #include <wiringPiI2C.h>
+#include <iostream>
 
-#define BOARD_ID_1 0x60 //device adress
-#define BOARD_ID_2 0x60 //device adress
+#define BOARD_ID_1 0x40 //first board adress
+#define BOARD_ID_2 0x60 //second board adress (check!)
 
-#define LED1_ON_L 0x3A  
-#define LED1_ON_H 0x3B 
-#define LED1_OFF_L 0x3C 
-#define LED1_OFF_H 0x3D 
+#define LED0_ON_L 0x3A // CHECK!!!!!!!
+#define LED0_ON_H 0x3B 
+#define LED0_OFF_L 0x3C 
+#define LED0_OFF_H 0x3D // AND THIS ALL!!!!!
 
-#define LED13_ON_L 0x3A //low ON register date 
-#define LED13_ON_H 0x3B //high ON register date
-#define LED13_OFF_L 0x3C //low OFF register date
-#define LED13_OFF_H 0x3D //high OFF register date
+void set_servo_angle (int channel, int ang) {
+     int board_1 {}, board_2 {};
+     int pwm {}, val_l {}, val_h {};
 
-#define LED13_ON_L 0x3A //low ON register date 
-#define LED13_ON_H 0x3B //high ON register date
-#define LED13_OFF_L 0x3C //low OFF register date
-#define LED13_OFF_H 0x3D //high OFF register date
+     if 0<=ang<=180 {
+          pwm = ang/180*4095;  //transform angle value to corresponding PWM
+          val_h = pwm >> 8;    //high 4 bits of PWM
+          val_l = pwm & 0xFF;  //low 8 bits of PWM
+     } else {
+          std::cout << "Impossible angle value!";
+     }
 
-#define LED13_ON_L 0x3A //low ON register date 
-#define LED13_ON_H 0x3B //high ON register date
-#define LED13_OFF_L 0x3C //low OFF register date
-#define LED13_OFF_H 0x3D //high OFF register date
-
-#define LED13_ON_L 0x3A //low ON register date 
-#define LED13_ON_H 0x3B //high ON register date
-#define LED13_OFF_L 0x3C //low OFF register date
-#define LED13_OFF_H 0x3D //high OFF register date
-
-#define LED13_ON_L 0x3A //low ON register date 
-#define LED13_ON_H 0x3B //high ON register date
-#define LED13_OFF_L 0x3C //low OFF register date
-#define LED13_OFF_H 0x3D //high OFF register date
-
-#define LED13_ON_L 0x3A //low ON register date 
-#define LED13_ON_H 0x3B //high ON register date
-#define LED13_OFF_L 0x3C //low OFF register date
-#define LED13_OFF_H 0x3D //high OFF register date
-
-#define LED13_ON_L 0x3A //low ON register date 
-#define LED13_ON_H 0x3B //high ON register date
-#define LED13_OFF_L 0x3C //low OFF register date
-#define LED13_OFF_H 0x3D //high OFF register date
-
-#define LED13_ON_L 0x3A //low ON register date 
-#define LED13_ON_H 0x3B //high ON register date
-#define LED13_OFF_L 0x3C //low OFF register date
-#define LED13_OFF_H 0x3D //high OFF register date
-
-#define LED13_ON_L 0x3A //low ON register date 
-#define LED13_ON_H 0x3B //high ON register date
-#define LED13_OFF_L 0x3C //low OFF register date
-#define LED13_OFF_H 0x3D //high OFF register date
-
-#define LED13_ON_L 0x3A //low ON register date 
-#define LED13_ON_H 0x3B //high ON register date
-#define LED13_OFF_L 0x3C //low OFF register date
-#define LED13_OFF_H 0x3D //high OFF register date
-
-#define LED13_ON_L 0x3A //low ON register date 
-#define LED13_ON_H 0x3B //high ON register date
-#define LED13_OFF_L 0x3C //low OFF register date
-#define LED13_OFF_H 0x3D //high OFF register date
-
-#define LED13_ON_L 0x3A //low ON register date 
-#define LED13_ON_H 0x3B //high ON register date
-#define LED13_OFF_L 0x3C //low OFF register date
-#define LED13_OFF_H 0x3D //high OFF register date
-
-#define LED13_ON_L 0x3A //low ON register date 
-#define LED13_ON_H 0x3B //high ON register date
-#define LED13_OFF_L 0x3C //low OFF register date
-#define LED13_OFF_H 0x3D //high OFF register date
-
-#define LED13_ON_L 0x3A //low ON register date 
-#define LED13_ON_H 0x3B //high ON register date
-#define LED13_OFF_L 0x3C //low OFF register date
-#define LED13_OFF_H 0x3D //high OFF register date
-
-#define LED13_ON_L 0x3A //low ON register date 
-#define LED13_ON_H 0x3B //high ON register date
-#define LED13_OFF_L 0x3C //low OFF register date
-#define LED13_OFF_H 0x3D //high OFF register date
-
-void set_servo_ang (int ang) {
-     int board, default_1, default_2, result_1, result_2;
-     int pwm, val_l, val_h;
-
-     pwm = ang/180*4095; //transform angle value to PWM
-     val_h = pwm & 0xF00; //high 4 bits of PWM
-     val_l = pwm & 0xFF; //low 8 bits of PWM
-
-     board = wiringPiI2CSetup (BOARD_ID); //initialisation
+     board_1 = wiringPiI2CSetup (BOARD_ID_1); //initialisation first board
+     board_2 = wiringPiI2CSetup (BOARD_ID_2); //initialisation second board
 
      /* Each Servo (LED) output have two 12-bit words, that indicates
      front and rear moments of every pulse (called ON and OFF moments).
@@ -101,11 +32,13 @@ void set_servo_ang (int ang) {
      bits and 4 high bits. Also have additional bits in  High register.
      Please, look to PCA9685 datasheet. And be attentive. */ 
 
-     //Start condition 
-     default_1 = wiringPiI2CWriteReg8 (BOARD_ID, LED13_ON_L, 0xA);
-     default_2 = wiringPiI2CWriteReg8 (BOARD_ID, LED13_ON_H, 0x0);
+     //Start condition (is default)
+     wiringPiI2CWriteReg8 (board, LED0_ON_L, 0xA);
+     wiringPiI2CWriteReg8 (board, LED0_ON_H, 0x0);
 
      //Stop condition
      result_1 = wiringPiI2CWriteReg8 (BOARD_ID, LED13_OFF_L, val_l);
      result_1 = wiringPiI2CWriteReg8 (BOARD_ID, LED13_OFF_H, val_h);
 }
+
+int read_servo_angle() {
