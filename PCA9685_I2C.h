03f8762aace9d0cdfeae2d3732a13c75d3a35cee
Paul <101666643+Scar092@users.noreply.github.com>
2022-07-08 20:19:45 +0300

    Update PCA9685_I2C.h

--- PCA9685_I2C.h
+++ PCA9685_I2C.h
@@ -2,8 +2,8 @@
 #define PCA9685_H
 
 int get_board_addr (int board);
+void board_init (int board);
 void set_servo_angle (int channel, int ang);
-void read_servo_angle (int channel);
-int board_
+int read_servo_angle (int channel);
 
 #endif
